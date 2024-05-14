893315dfe75180def34144e1916483530232c3fe
jf7689 <jf7689@rit.edu>
2024-05-14 17:21:05 -0400

    Able to fetch app access token

--- src/index.js
+++ src/index.js
@@ -7,9 +7,19 @@ import usersRouter from "./routes/users.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const getCred = async () => {
+    let cred = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=client_credentials`,
+    {
+        method: "POST",
+    });
+    let res = await cred.json();
+    console.log(res);
+}
+
 app.use(usersRouter);
 
 app.get("/", (req, res) => {
+    getCred()
     res.status(200).send({ msg: "Hello" });
 })
 

c27bc912d73a428161299097e11938823f367838
jf7689 <jf7689@rit.edu>
2024-05-20 16:55:49 -0400

    Fetching and returning vods working

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -17,6 +17,7 @@ const getVods = async (req, res) => {
     const { userId } = req.params;
     let vods = []
 
+    // Get initital vods
     let resp = await fetch(`https://api.twitch.tv/helix/videos?user_id=${userId}&type=archive&first=30`, 
     {
         method: "GET",
@@ -27,7 +28,36 @@ const getVods = async (req, res) => {
     })
 
     let respData = await resp.json();
-    vods = [...respData.data]
+    vods = [...respData.data];
+
+    // Check for next page of vods
+    let cursor = "";
+    let paginationObj = respData.pagination;
+    if (paginationObj) {
+        cursor = paginationObj.cursor;
+    }
+    
+    // Loop through pagination for vods
+    while (cursor) { 
+        resp = await fetch(`https://api.twitch.tv/helix/videos?user_id=${userId}&type=archive&first=100&after=${cursor}`, 
+        {
+            method: "GET",
+            headers: {            
+                "Client-ID": process.env.CLIENT_ID,                
+                "Authorization": `Bearer ${process.env.ACCESS_TOKEN}`           
+            }
+        })
+
+        respData = await resp.json();
+        vods = [...vods, ...respData.data];
+
+        // Check for next page of vods
+        paginationObj = respData.pagination;
+        if (paginationObj) {
+            cursor = paginationObj.cursor;
+        }
+    }
+
     res.status(200).send(vods);
 }
 

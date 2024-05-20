cc7c659f244928f10b8b141d8de0898371336d0b
jf7689 <jf7689@rit.edu>
2024-05-20 13:00:04 -0400

    Start route for getting vods

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,7 @@
 const getID = async (req, res) => {
     const { name } = req.params;
-    let resp = await fetch(`https://api.twitch.tv/helix/users?login=${name}`, {
+    let resp = await fetch(`https://api.twitch.tv/helix/users?login=${name}`, 
+    {
         method: "GET",
         headers: {            
             "Client-ID": process.env.CLIENT_ID,                
@@ -12,4 +13,22 @@ const getID = async (req, res) => {
     res.status(200).send({ id: respData.data[0].id });
 }
 
-export { getID };
+const getVods = async (req, res) => {
+    const { userId } = req.params;
+    let vods = []
+
+    let resp = await fetch(`https://api.twitch.tv/helix/videos?user_id=${userId}&type=archive&first=30`, 
+    {
+        method: "GET",
+        headers: {            
+            "Client-ID": process.env.CLIENT_ID,                
+            "Authorization": `Bearer ${process.env.ACCESS_TOKEN}`           
+        }
+    })
+
+    let respData = await resp.json();
+    vods = [...respData.data]
+    res.status(200).send(vods);
+}
+
+export { getID, getVods };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getID } from "../controllers/users.js";
+import { getID, getVods } from "../controllers/users.js";
 
 const router = Router();
 
 router.get("/api/users/:name", getID);
+router.get("/api/users/vods/:userId", getVods);
 
 export default router;
